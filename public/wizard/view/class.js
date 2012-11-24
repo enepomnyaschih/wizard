9161913658005379b7d6e3519c1523426dc8d62a
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2012-11-24 15:38:12 +0700

    #1: Cleaned class form.

--- public/wizard/view/class.js
+++ public/wizard/view/class.js
@@ -11,19 +11,7 @@
 	
 	render: function() {
 		this._super();
-		
 		this.deleteEl.click(JW.Function.inScope(this._onDeleteClick, this));
-		/*
-		this.namePicker = new wizard.view.picker.Name({
-			validator      : JW.Function.inScope(this._validateName, this),
-			applier        : JW.Function.inScope(this._applyName, this),
-			value          : this.module.name,
-			renderParent   : this,
-			renderPosition : "name-picker"
-		});
-		*/
-		this.el.find(".label").css("width", "100px");
-		
 		this.editor = new wizard.view.Editor({
 			model : this.model
 		});
@@ -42,7 +30,8 @@
 	
 	afterAppend: function() {
 		this._super();
-		//this.namePicker.focus();
+		this.editor._skipClickFocus = true;
+		this.element.focus();
 	},
 	
 	destroyComponent: function() {
@@ -52,23 +41,5 @@
 	
 	_onDeleteClick: function() {
 		this.module.remove();
-	},
-	
-	_validateName: function(name) {
-		if (!wizard.view.Class.nameRegex.test(name)) {
-			return "Invalid class name. Must contain liters and numbers only, must start from higher case liter.";
-		}
-		var isFree = this.module.parent.classes.every(function(sibling) {
-			return (sibling === this.module) || (sibling.name !== name);
-		}, this);
-		if (!isFree) {
-			return "Class with such name exists already.";
-		}
-	},
-	
-	_applyName: function(name) {
-		this.module.setName(name);
 	}
 });
-
-wizard.view.Class.nameRegex = /^[A-Z][A-Za-z0-9]*$/;
